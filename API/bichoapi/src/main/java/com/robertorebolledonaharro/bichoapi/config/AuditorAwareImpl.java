acfb21ec3ac7ed8903ee7a402fb652a5132acab7
Roberto Rebolledo <robertorebolledo151@gmail.com>
2024-03-18 18:06:27 +0100

    cors

---
+++ API/bichoapi/src/main/java/com/robertorebolledonaharro/bichoapi/config/AuditorAwareImpl.java
@@ -0,0 +1,29 @@
+package com.robertorebolledonaharro.bichoapi.config;
+
+import com.robertorebolledonaharro.bichoapi.user.model.User;
+import lombok.extern.java.Log;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Log
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .filter(principal -> principal instanceof User)
+                .map(User.class::cast)
+                .map(User::getId)
+                .map(UUID::toString);
+
+
+    }
+}
