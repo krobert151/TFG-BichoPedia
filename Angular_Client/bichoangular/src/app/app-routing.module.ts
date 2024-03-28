60949e5629d2dd8a907be4f0d02fb9f4efeb67c5
Roberto Rebolledo <robertorebolledo151@gmail.com>
2024-03-28 14:32:56 +0100

    actualiza pero aun falta decorar y validar un poco

--- Angular_Client/bichoangular/src/app/app-routing.module.ts
+++ Angular_Client/bichoangular/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/user/login/login.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { SpecieComponent } from './components/sepecies/species/species.component';
+import { SpecieComponent } from './components/species/species-table/species.component';
+import { EditSpecieComponent } from './components/species/edit-specie/edit-specie.component';
 
 const routes: Routes = [
 
   { path: 'login', component: LoginComponent}, 
   { path: 'home-page',component:HomePageComponent}, 
   { path: 'species',component:SpecieComponent}, 
+  { path: 'species/edit',component:EditSpecieComponent}, 
   { path: '', pathMatch: 'full', redirectTo: '/login' },
 
 
--- Angular_Client/bichoangular/src/app/app.module.ts
+++ Angular_Client/bichoangular/src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ImportModule } from './components/import/import.module';
+
+import { SpecieComponent } from './components/species/species-table/species.component';
+import { EditSpecieComponent } from './components/species/edit-specie/edit-specie.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,14 @@ import { ImportModule } from './components/import/import.module';
     LoginComponent,
     HomePageComponent,
     NavbarComponent,
+    SpecieComponent,
+    EditSpecieComponent    
   ],
   imports: [
+    FormsModule,
     ImportModule,
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule ,
     ReactiveFormsModule,
     HttpClientModule,
     NgbModule,
